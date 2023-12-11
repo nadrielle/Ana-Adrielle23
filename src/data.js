05058ed0af6e2c5d1824ebf7aa0d76686994c957
nadrielle <129507179+nadrielle@users.noreply.github.com>
2023-12-11 14:53:03 -0300

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,91 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://purepng.com/public/uploads/large/purepng.com-porscheporscheporsche-automobilesporsche-carsporsche-brand-1701527596497untbm.png',
+        title: 'PORSHE',
+        desc: 'Somos Melhores Juntos',
+        bg: '#fff',
+    },
+    {
+        id: 2,
+        img: 'https://purepng.com/public/uploads/large/purepng.com-porscheporscheporsche-automobilesporsche-carsporsche-brand-1701527596497untbm.png',
+        title: 'Ofertas Imperdíveis',
+        desc: 'Somos Melhores Juntos',
+        bg: '#ffbaba',
+    },
+    {
+        id: 3,
+        img: 'https://purepng.com/public/uploads/large/purepng.com-porscheporscheporsche-automobilesporsche-carsporsche-brand-1701527596497untbm.png',
+        title: 'Queridinhos da Galera',
+        desc: 'Somos Melhores Juntos',
+        bg: '#ff99ba',
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: 'https://lh3.googleusercontent.com/-dxeT7XqTtNQ/YvpxXUL-JOI/AAAAAAAASgg/Iw182RgFBS4uX60dY1akIR_RkCLt-DQ0QCNcBGAsYHQ/s1600/1660580184815903-0.png',
+        title: '718',
+    },
+    {
+        id: 2,
+        img: 'https://lh3.googleusercontent.com/-dxeT7XqTtNQ/YvpxXUL-JOI/AAAAAAAASgg/Iw182RgFBS4uX60dY1akIR_RkCLt-DQ0QCNcBGAsYHQ/s1600/1660580184815903-0.png',
+        title: '911',
+    },
+    {
+        id: 3,
+        img: 'https://lh3.googleusercontent.com/-dxeT7XqTtNQ/YvpxXUL-JOI/AAAAAAAASgg/Iw182RgFBS4uX60dY1akIR_RkCLt-DQ0QCNcBGAsYHQ/s1600/1660580184815903-0.png',
+        title: 'Taycan',
+    },
+    {
+        id: 4,
+        img: 'https://lh3.googleusercontent.com/-dxeT7XqTtNQ/YvpxXUL-JOI/AAAAAAAASgg/Iw182RgFBS4uX60dY1akIR_RkCLt-DQ0QCNcBGAsYHQ/s1600/1660580184815903-0.png',
+        title: 'Panamera',
+    },
+    {
+        id: 5,
+        img: 'https://lh3.googleusercontent.com/-dxeT7XqTtNQ/YvpxXUL-JOI/AAAAAAAASgg/Iw182RgFBS4uX60dY1akIR_RkCLt-DQ0QCNcBGAsYHQ/s1600/1660580184815903-0.png',
+        title: 'Macan',
+    },
+    {
+        id: 6,
+        img: 'https://lh3.googleusercontent.com/-dxeT7XqTtNQ/YvpxXUL-JOI/AAAAAAAASgg/Iw182RgFBS4uX60dY1akIR_RkCLt-DQ0QCNcBGAsYHQ/s1600/1660580184815903-0.png',
+        title: 'Cayenne',
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 2,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 3,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 4,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 5,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 6,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 7,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+    {
+        id: 8,
+        img: 'https://newsroom.porsche.com/dam/jcr:a388376a-c5e2-4363-939b-6fbacd867a64/718_neu_2023.png',
+    },
+];
